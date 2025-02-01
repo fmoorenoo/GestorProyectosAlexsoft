515386848fb872ae0c0fe3971d6ca8d4711c38e6
fmoorenoo <fermontemore@gmail.com>
2025-02-01 18:49:06 +0000

    ProjectScreen empezada

---
+++ src/main/kotlin/ProjectScreen.kt
@@ -0,0 +1,32 @@
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class ProjectScreen(private val project: Project) : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(project.name) },
+                    navigationIcon = {
+                        IconButton(onClick = { navigator.pop() }) {
+                            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Volver")
+                        }
+                    },
+                    backgroundColor = Color(0xFF0A9396),
+                    contentColor = Color.White
+                )
+            },
+            backgroundColor = Color(0xFFF0F0F0)
+        ) {
+        }
+    }
+}
