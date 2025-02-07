4000f44c354219f7d03cb96d2678a53349e427a3
fmoorenoo <fermontemore@gmail.com>
2025-02-07 22:53:19 +0000

    Mostrar las tareas de un proyecto en un LazyRow

--- src/main/kotlin/screens/ProjectScreen.kt
+++ src/main/kotlin/screens/ProjectScreen.kt
@@ -1,16 +1,19 @@
 package screens
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,12 +24,33 @@ import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import kotlinx.coroutines.launch
 import models.Project
+import models.Task
+import network.apiGetTasksByProject
 
 class ProjectScreen(private val project: Project) : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        var tasks by remember { mutableStateOf<List<Task>>(emptyList()) }
+        var isLoading by remember { mutableStateOf(true) }
+
+        val scrollState = rememberLazyListState()
+        val coroutineScope = rememberCoroutineScope()
+
+        LaunchedEffect(project.id) {
+            apiGetTasksByProject(
+                projectId = project.id,
+                onSuccessResponse = { fetchedTasks ->
+                    tasks = fetchedTasks
+                    isLoading = false
+                },
+                onError = {
+                    isLoading = false
+                }
+            )
+        }
 
         Scaffold(
             topBar = {
@@ -55,39 +79,24 @@ class ProjectScreen(private val project: Project) : Screen {
                     elevation = 4.dp,
                     backgroundColor = Color.White
                 ) {
-                    Column(
-                        modifier = Modifier.padding(16.dp)
-                    ) {
+                    Column(modifier = Modifier.padding(16.dp)) {
                         Text(
                             text = "Nombre: " + project.nombre,
-                            style = TextStyle(
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold,
-                                color = Color(0xFF005F73)
-                            )
+                            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color(0xFF005F73))
                         )
                         Spacer(modifier = Modifier.height(8.dp))
                         Text(
                             text = "Fecha de inicio: " + project.fechaInicio,
-                            style = TextStyle(
-                                fontSize = 16.sp,
-                                color = Color(0xFF0A9396)
-                            )
+                            style = TextStyle(fontSize = 16.sp, color = Color(0xFF0A9396))
                         )
                         Text(
                             text = "Fecha de finalización: " + project.fechaFinalizacion,
-                            style = TextStyle(
-                                fontSize = 16.sp,
-                                color = Color(0xFF0A9396)
-                            )
+                            style = TextStyle(fontSize = 16.sp, color = Color(0xFF0A9396))
                         )
                         Spacer(modifier = Modifier.height(8.dp))
                         Text(
                             text = "Descripción: " + project.descripcion,
-                            style = TextStyle(
-                                fontSize = 14.sp,
-                                color = Color.Black
-                            )
+                            style = TextStyle(fontSize = 14.sp, color = Color.Black)
                         )
                     }
                 }
@@ -96,95 +105,49 @@ class ProjectScreen(private val project: Project) : Screen {
 
                 Text(
                     text = "Tareas del Proyecto",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color(0xFF005F73)
-                    ),
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color(0xFF005F73)),
                     modifier = Modifier.padding(bottom = 8.dp)
                 )
 
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                ) {
-                    items(listOf("Tarea 1", "Tarea 2", "Tarea 3")) { task ->
-                        TaskItem(taskName = task, onClick = {
-                            navigator.push(TasksScreen())
-                        })
-                        Spacer(modifier = Modifier.height(8.dp))
+                if (isLoading) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+                } else if (tasks.isEmpty()) {
+                    Text(
+                        text = "No hay tareas asignadas",
+                        style = TextStyle(fontSize = 16.sp, color = Color.Gray),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                } else {
+                    LazyRow(
+                        state = scrollState,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .draggable(
+                                orientation = Orientation.Horizontal,
+                                state = rememberDraggableState { delta ->
+                                    coroutineScope.launch {
+                                        scrollState.scrollBy(-delta)
+                                    }
+                                }
+                            ),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(tasks) { task ->
+                            TaskItem(task = task)
+                        }
                     }
                 }
 
                 Button(
                     onClick = {
                     },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
                     colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF0A9396))
                 ) {
                     Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            imageVector = Icons.Filled.Add,
-                            contentDescription = "Añadir tarea",
-                            tint = Color.White
-                        )
+                        Icon(imageVector = Icons.Filled.Add, contentDescription = "Añadir tarea", tint = Color.White)
                         Spacer(modifier = Modifier.width(8.dp))
-                        Text(
-                            text = "Añadir Tarea",
-                            style = TextStyle(
-                                fontSize = 18.sp,
-                                color = Color.White
-                            )
-                        )
-                    }
-                }
-
-                Text(
-                    text = "Programadores Asignados",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color(0xFF005F73)
-                    ),
-                    modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
-                )
-
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                ) {
-                    items(listOf("Programador 1", "Programador 2")) { programmer ->
-                        ProgrammerItem(programmer = programmer)
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
-                }
-
-                Button(
-                    onClick = {
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF0A9396))
-                ) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Icon(
-                            imageVector = Icons.Filled.Person,
-                            contentDescription = "Asignar programadores",
-                            tint = Color.White
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(
-                            text = "Asignar Programadores",
-                            style = TextStyle(
-                                fontSize = 18.sp,
-                                color = Color.White
-                            )
-                        )
+                        Text(text = "Añadir Tarea", style = TextStyle(fontSize = 18.sp, color = Color.White))
                     }
                 }
             }
@@ -193,70 +156,27 @@ class ProjectScreen(private val project: Project) : Screen {
 }
 
 @Composable
-fun TaskItem(taskName: String, onClick: () -> Unit) {
+fun TaskItem(task: Task) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onClick() },
+        modifier = Modifier.width(220.dp), // Se ajusta el tamaño de la tarjeta
         shape = RoundedCornerShape(12.dp),
         elevation = 4.dp,
         backgroundColor = Color.White
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
+        Column(modifier = Modifier.padding(16.dp)) {
             Text(
-                text = taskName,
-                style = TextStyle(
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF005F73)
-                )
+                text = task.nombre,
+                style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold, color = Color(0xFF005F73))
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "Descripción de la tarea...",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    color = Color.Black
-                )
+                text = task.descripcion,
+                style = TextStyle(fontSize = 14.sp, color = Color.Black)
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = "Programadores: Programador 1, Programador 2",
-                style = TextStyle(
-                    fontSize = 12.sp,
-                    color = Color.Gray
-                )
-            )
-        }
-    }
-}
-
-@Composable
-fun ProgrammerItem(programmer: String) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(12.dp),
-        elevation = 4.dp,
-        backgroundColor = Color.White
-    ) {
-        Row(
-            modifier = Modifier.padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Person,
-                contentDescription = "Programador",
-                tint = Color(0xFF0A9396)
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = programmer,
-                style = TextStyle(
-                    fontSize = 16.sp,
-                    color = Color.Black
-                )
+                text = "Programador ID: ${task.programador}",
+                style = TextStyle(fontSize = 12.sp, color = Color.Gray)
             )
         }
     }
