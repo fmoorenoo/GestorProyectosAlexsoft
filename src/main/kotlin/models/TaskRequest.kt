6fcfc2ee02917afc791042c0f0b23c9514eca983
fmoorenoo <fermontemore@gmail.com>
2025-02-07 22:51:22 +0000

    Modelo para crear tareas

---
+++ src/main/kotlin/models/TaskRequest.kt
@@ -0,0 +1,14 @@
+package models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TaskRequest(
+    val nombre: String,
+    val descripcion: String,
+    val estimacion: Int,
+    val fecha_creacion: String,
+    val fecha_finalizacion: String?,
+    val programador: Int,
+    val proyecto: Int
+)
