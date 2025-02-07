a73f40ebc65466080ae733d4ffa3187967c62297
fmoorenoo <fermontemore@gmail.com>
2025-02-07 22:50:11 +0000

    Función para el método de crear tareas de la API

---
+++ src/main/kotlin/network/CreateTasksAPI.kt
@@ -0,0 +1,52 @@
+package network
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import network.NetworkUtils.httpClient
+import models.TaskRequest
+
+
+fun apiCreateTask(
+    nombre: String,
+    descripcion: String,
+    estimacion: Int,
+    fechaCreacion: String,
+    fechaFinalizacion: String?,
+    programadorId: Int,
+    proyectoId: Int,
+    onSuccessResponse: (String) -> Unit,
+    onError: (String) -> Unit
+) {
+    val url = "http://127.0.0.1:5000/crear_tareas"
+    val requestBody = TaskRequest(
+        nombre = nombre,
+        descripcion = descripcion,
+        estimacion = estimacion,
+        fecha_creacion = fechaCreacion,
+        fecha_finalizacion = fechaFinalizacion,
+        programador = programadorId,
+        proyecto = proyectoId
+    )
+
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val response = httpClient.post(url) {
+                contentType(ContentType.Application.Json)
+                setBody(requestBody)
+            }
+            val responseBody = response.bodyAsText()
+
+            if (response.status == HttpStatusCode.Created) {
+                onSuccessResponse(responseBody)
+            } else {
+                onError("Error: ${response.status}, Body: $responseBody")
+            }
+        } catch (e: Exception) {
+            onError("Exception: ${e.message}")
+        }
+    }
+}
