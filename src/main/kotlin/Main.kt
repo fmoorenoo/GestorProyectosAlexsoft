962d98d6d13e5990cb431dc54f6bce83e4efab2b
fmoorenoo <fermontemore@gmail.com>
2025-02-08 12:54:44 +0000

    Dimensiones de la ventana modificadas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.unit.DpSize
 import cafe.adriel.voyager.navigator.Navigator
 import screens.LoginScreen
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Gestor de Proyectos",
+        state = WindowState(size = DpSize(800.dp, 800.dp))
+    ) {
         Navigator(LoginScreen())
     }
 }
